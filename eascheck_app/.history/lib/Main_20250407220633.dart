455500deb756ee0c7c5cfc7564bd3cfc12ffd6b0
Peerapanname <peekmail198@gmail.com>
2025-04-08 01:34:02 +0700

    เทสรอบ3

---
+++ eascheck_app/.history/lib/Main_20250407220633.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EascheckApp());
+}
+
+class EascheckApp extends StatelessWidget {
+  const EascheckApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Eascheck',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Eascheck 🥗'),
+      ),
+      body: const Center(
+        child: Text('สวัสดี! Eascheck พร้อมลุยแล้ว 🚀'),
+      ),
+    );
+  }
+}
